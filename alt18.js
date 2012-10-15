7849552cba32539cdf0c76d661a974cd0c3ba417
Phips Peter <pspeter333@gmail.com>
2012-10-15 10:56:55 -0700

    Passing the basic case

--- alt18.js
+++ alt18.js
@@ -3,12 +3,20 @@ var scraper = require('scraper');
 
 // Parse a single line
 var parseLine = function(line) {
-  var match = line.match(/(\d{,2})\.\s+(.+?)\s+-\s+\"(.+?)\"\s*$/);
-  // Check for invalid input
-  if (match === null) {
-    return match;
-  }
-};
+    var match = line.match(/(\d{1,2})\.\s+(.+?)\s+-\s+\"(.+?)\"\s*$/);
+    // Check for invalid input
+    if(match === null) {
+      return match;
+    }
+    // Parse input if valid
+    var data = {
+      rank: parseInt(match[1]),
+      artist: match[2],
+      track: match[3]
+    }
+
+    return data;
+  };
 
 // Export definition
 exports.parseLine = parseLine;

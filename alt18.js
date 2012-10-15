ab4255c1cecc362ebae191ab85221406126baa32
Phips Peter <pspeter333@gmail.com>
2012-10-15 10:55:03 -0700

    Updated the tests to handle valid line

--- alt18.js
+++ alt18.js
@@ -3,5 +3,12 @@ var scraper = require('scraper');
 
 // Parse a single line
 var parseLine = function(line) {
-  
+  var match = line.match(/(\d{,2})\.\s+(.+?)\s+-\s+\"(.+?)\"\s*$/);
+  // Check for invalid input
+  if (match === null) {
+    return match;
+  }
 };
+
+// Export definition
+exports.parseLine = parseLine;
--- test.js
+++ test.js
@@ -1,13 +1,19 @@
 // Dependencies
 var assert = require('assert'),
   vows = require('vows'),
-  alt18 = require('alt18');
+  alt18 = require('./alt18');
 
 // Create test suite
 vows.describe('Alt18').addBatch({
   'when parsing a line': {
     'should hanlde an empty line': function() {
-      assert(alt18.parseLine(''), {});
+      assert.equal(alt18.parseLine(''), null);
+    },
+    'should handle an a valid line': function() {
+      var data = alt18.parseLine('18. The Neighbourhood - "Female Robbery"');
+      assert.equal(data.rank, 18);
+      assert.equal(data.artist, 'The Neighbourhood');
+      assert.equal(data.track, 'Female Robbery');
     }
   }
 }).run();
